9622b158a1c8b088f9e13243b61699d859cbf55a
eugeneer-code <evgeniy.tsvet@gmail.com>
2022-01-23 22:23:57 +0300

    add keepass implementation

---
+++ src/util.cc
@@ -0,0 +1,98 @@
+/*
+ * libkeepass - KeePass key database importer/exporter
+ * Copyright (C) 2014 Christian Kindahl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "util.hh"
+
+#include <cassert>
+#include <random>
+#include <time.h>
+#include <iomanip>
+#include <sstream>
+
+namespace keepass {
+
+std::string time_to_str(const std::time_t& time) {
+  const std::tm* local_time = std::localtime(&time);
+  assert(local_time != nullptr);
+
+  char buffer[128];
+  strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", local_time);
+  return buffer;
+}
+
+char* strptime(const char* s, const char* f, struct tm* tm) {
+    // Isn't the C++ standard lib nice? std::get_time is defined such that its
+    // format parameters are the exact same as strptime. Of course, we have to
+    // create a string stream first, and imbue it with the current C locale, and
+    // we also have to make sure we return the right things if it fails, or
+    // if it succeeds, but this is still far simpler an implementation than any
+    // of the versions in any of the C standard libraries.
+    std::istringstream input(s);
+    input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+    input >> std::get_time(tm, f);
+    if (input.fail()) {
+        return nullptr;
+    }
+    return (char*)(s + input.tellg());
+}
+
+int days_from_epoch(int y, int m, int d)
+{
+    y -= m <= 2;
+    int era = y / 400;
+    int yoe = y - era * 400;                                   // [0, 399]
+    int doy = (153 * (m + (m > 2 ? -3 : 9)) + 2) / 5 + d - 1;  // [0, 365]
+    int doe = yoe * 365 + yoe / 4 - yoe / 100 + doy;           // [0, 146096]
+    return era * 146097 + doe - 719468;
+}
+
+// It  does not modify broken-down time
+time_t timegm(struct tm const* t)
+{
+    int year = t->tm_year + 1900;
+    int month = t->tm_mon;          // 0-11
+    if (month > 11)
+    {
+        year += month / 12;
+        month %= 12;
+    }
+    else if (month < 0)
+    {
+        int years_diff = (11 - month) / 12;
+        year -= years_diff;
+        month += 12 * years_diff;
+    }
+    int days_since_epoch = days_from_epoch(year, month + 1, t->tm_mday);
+    return 60 * (60 * (24L * days_since_epoch + t->tm_hour) + t->tm_min) + t->tm_sec;
+}
+
+std::array<uint8_t, 16> generate_uuid() {
+  std::random_device rd;
+  std::mt19937 engine(rd());
+
+  std::uniform_int_distribution<uint8_t> uniform_dist(0, 255);
+
+  // Fill block with random values.
+  std::array<uint8_t, 16> uuid;
+  for (std::size_t i = 0; i < 16; ++i)
+    uuid[i] = uniform_dist(engine);
+
+  return uuid;
+}
+
+}   // namespace keepass
