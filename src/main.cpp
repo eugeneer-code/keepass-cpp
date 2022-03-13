4b893d94bd1c5a746b217ce7f4f715bcf16b9849
eugeneer-code <evgeniy.tsvet@gmail.com>
2022-03-14 00:14:15 +0300

    add cmake key for building example

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,20 @@ int main()
 {
     keepass::Key key("password");
 
+
     try {
         keepass::KdbxFile file;
         std::unique_ptr<keepass::Database> db = file.Import("./Database.kdbx", key);
 
-        std::cout << db->root()->Entries().front().get()->ToJson();
+        auto groups = db->root()->Groups();
+        for(auto group : groups){
+            std::cout << "\nGroup " << group->name() << group->notes() << ", entries: " << group->Entries().size();
+        }
+
+        auto entries = db->root()->Entries();
+        for(auto entry : entries){
+            std::cout << "\nEntry " << entry->title().value() << entry->username().value() << entry->password().value();
+        }
 
     }
     catch(std::exception& e){
